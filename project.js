b0199a6421528ca8a11e2bf93c04a6a366d08196
madiha-developer24 <64437935+madiha-developer24@users.noreply.github.com>
2020-06-07 04:34:51 -0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,35 @@
+
+$(document).ready(function(){
+
+    
+  $('.autoplay').slick(
+
+    
+{
+ dots: true,
+//  arrows:false
+autoplay: true,
+autoplaySpeed: 2000,
+
+}
+
+
+    
+  );
+
+    //       $("#share").jsSocials({
+    //     shares: ["email", "twitter", "facebook", "googleplus", "linkedin", "pinterest", "stumbleupon", "whatsapp"]
+    // });
+
+    
+
+
+
+    
+    
+    
+
+    
+
+});
+
